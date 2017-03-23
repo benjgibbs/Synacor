ca64562aca40c76fdc0dd594d318a945d56343cf
Ben Gibbs <benjgibbs@gmail.com>
2017-03-23 07:10:55 +0000

    pre memory refactor

--- challenge.py
+++ challenge.py
@@ -15,28 +15,32 @@ def load():
 
 class Vm:
     def __init__(self, prog):
+        self.trace = False
         self.reg = 8 * [0]
         self.mem = list(prog) #= [0] * 2**15
         self.stack = list()
         self.pc = 0  
-    
+
     # halt: 0
     def halt(self,tr):
         print ('End')
-        tr.write('halt\n' )
+        if self.trace:
+            tr.write('halt\n' )
         self.pc = MAX
-        
+
     # set: 1 a b
     def set(self, tr):
         a, b = self.reg_addr(1), self.read(2)
-        tr.write('set:   %5d, %5d\n' % (a, b) )
+        if self.trace:
+            tr.write('set:   %5d, %5d\n' % (a, b) )
         self.reg[a] = b
         self.pc += 3
     
     # push: 2 a
     def push(self, tr):
         a = self.read(1)
-        tr.write('push:  %5d\n' % a )
+        if self.trace:
+            tr.write('push:  %5d\n' % a )
         self.stack.append(a)
         self.pc += 2
     
@@ -44,34 +48,39 @@ class Vm:
     def pop(self, tr):
         a = self.reg_addr(1)
         p = self.stack.pop()
-        tr.write('pop:   %5d (%5d)\n' % (a, p) )
+        if self.trace:
+            tr.write('pop:   %5d (%5d)\n' % (a, p) )
         self.reg[a] = p
         self.pc += 2
 
     # eq: 4 a b c
     def eq(self, tr):
         a, b, c = self.reg_addr(1), self.read(2), self.read(3)
-        tr.write('eq:    %5d, %5d, %5d\n' % (a, b, c) )
+        if self.trace:
+            tr.write('eq:    %5d, %5d, %5d\n' % (a, b, c) )
         self.reg[a] = 1 if b == c else 0
         self.pc += 4
         
     # gt: 5 a b c
     def gt(self, tr):
         a, b, c = self.reg_addr(1), self.read(2), self.read(3)
-        tr.write('gt:    %5d, %5d, %5d\n' % (a, b, c))
+        if self.trace: 
+            tr.write('gt:    %5d, %5d, %5d\n' % (a, b, c))
         self.reg[a] = 1 if b > c else 0
         self.pc += 4
     
     # jmp: 6 a
     def jmp(self, tr):
         a = self.read(1)
-        tr.write('jmp:   %5d\n' % (a))
+        if self.trace:
+            tr.write('jmp:   %5d\n' % (a))
         self.pc = a
 
     # jt: 7 a b
     def jt(self, tr):
         a, b = self.read(1), self.read(2)
-        tr.write('jt:    %5d, %5d\n' % (a, b))
+        if self.trace:
+            tr.write('jt:    %5d, %5d\n' % (a, b))
         if a != 0:
             self.pc = b            
         else:
@@ -80,7 +89,8 @@ class Vm:
     # jf: 8 a b
     def jf(self, tr):
         a, b = self.read(1), self.read(2)
-        tr.write('jf:    %5d, %5d\n' % (a, b))
+        if self.trace:
+            tr.write('jf:    %5d, %5d\n' % (a, b))
         if a == 0:
             self.pc = b            
         else:
@@ -90,7 +100,8 @@ class Vm:
     # assign into <a> the sum of <b> and <c> (modulo 32768)
     def add(self, tr):
         a, b, c = self.reg_addr(1), self.read(2), self.read(3)
-        tr.write('add:   %5d, %5d, %5d\n' % (a, b, c) )
+        if self.trace:
+            tr.write('add:   %5d, %5d, %5d\n' % (a, b, c) )
         self.reg[a] = (b+c) % MAX
         self.pc += 4
 
@@ -98,7 +109,8 @@ class Vm:
     # store into <a> the product of <b> and <c> (modulo 32768)
     def mult(self,tr):
         a, b, c = self.reg_addr(1), self.read(2), self.read(3)
-        tr.write('mult:  %5d, %5d, %5d\n' % (a, b, c) )
+        if self.trace:
+            tr.write('mult:  %5d, %5d, %5d\n' % (a, b, c) )
         self.reg[a] = (b * c) % MAX
         self.pc += 4
 
@@ -106,7 +118,8 @@ class Vm:
     # store into <a> the remainder of <b> divided by <c>
     def mod(self,tr):
         a, b, c = self.reg_addr(1), self.read(2), self.read(3)
-        tr.write('mod:   %5d, %5d, %5d\n' % (a, b, c) )
+        if self.trace:
+            tr.write('mod:   %5d, %5d, %5d\n' % (a, b, c) )
         self.reg[a] = b % c
         self.pc += 4
 
@@ -114,7 +127,8 @@ class Vm:
     # stores into <a> the bitwise and of <b> and <c>
     def andfn(self, tr): 
         a, b, c = self.reg_addr(1), self.read(2), self.read(3)
-        tr.write('add:   %5d, %5d, %5d\n' % (a, b, c) )
+        if self.trace:
+            tr.write('add:   %5d, %5d, %5d\n' % (a, b, c) )
         self.reg[a] = b & c
         self.pc += 4
 
@@ -122,7 +136,8 @@ class Vm:
     # stores into <a> the bitwise or of <b> and <c>
     def orfn(self, tr): 
         a, b, c = self.reg_addr(1), self.read(2), self.read(3)
-        tr.write('or:    %5d, %5d, %5d\n' % (a, b, c) )
+        if self.trace:
+            tr.write('or:    %5d, %5d, %5d\n' % (a, b, c) )
         self.reg[a] = b | c
         self.pc += 4
 
@@ -131,7 +146,8 @@ class Vm:
     # '{0:b}'.format(~int("1010",2) & 32767) -> '111111111110101'
     def notfn(self, tr): 
         a, b = self.reg_addr(1), self.read(2)
-        tr.write('not:   %5d, %5d\n' % (a, b) )
+        if self.trace:
+            tr.write('not:   %5d, %5d\n' % (a, b) )
         self.reg[a] = ~b & (2**15-1)
         self.pc += 3
     
@@ -139,7 +155,8 @@ class Vm:
     # read memory at address <b> and write it to <a>  (mod 32768)
     def rmem(self, tr): 
         a, b = self.mem[self.pc + 1], self.mem[self.pc + 2]
-        tr.write('rmem:  %5d, %5d\n' % (a, b))
+        if self.trace:
+            tr.write('rmem:  %5d, %5d\n' % (a, b))
         self.reg[a % MAX] = \
             self.mem[b] if b < MAX else self.mem[self.reg[b % MAX]]
         self.pc += 3
@@ -148,7 +165,8 @@ class Vm:
     # write the value from <b> into memory at address <a>
     def wmem(self, tr): 
         a, b = self.mem[self.pc + 1], self.mem[self.pc + 2]
-        tr.write('wmem:  %5d, %5d\n' % (a, b))
+        if self.trace:
+            tr.write('wmem:  %5d, %5d\n' % (a, b))
         v = b if b < MAX else self.reg[b % MAX]
 
         if a < MAX:
@@ -162,7 +180,8 @@ class Vm:
     # write the address of the next instruction to the stack and jump to <a>
     def call(self, tr):
         a = self.read(1)
-        tr.write('call:  %5d\n' % (a) )
+        if self.trace:
+            tr.write('call:  %5d\n' % (a) )
         self.stack.append(self.pc+2)
         self.pc = a
 
@@ -170,7 +189,8 @@ class Vm:
     # remove the top element from the stack and jump to it; empty stack = halt
     def ret(self, tr):
         self.pc = self.stack.pop()
-        tr.write('ret:   %5d\n' % (self.pc) )
+        if self.trace:
+            tr.write('ret:   %5d\n' % (self.pc) )
     
     # out: 19 a
     def out(self, tr):
@@ -180,7 +200,8 @@ class Vm:
         if c == '\n':
             c = '\\n'
 
-        tr.write('out:   %5d (%s)\n' % (a, c) )
+        if self.trace:
+            tr.write('out:   %5d (%s)\n' % (a, c) )
         stdout.write('%c' % chr(a))
         self.pc += 2
 
@@ -192,13 +213,15 @@ class Vm:
     def readin(self, tr):
         a = self.reg_addr(1)
         c = stdin.read(1)
-        tr.write('in:    %5d (%3d,%c)\n' % (a, ord(c), c) )
+        if self.trace:
+            tr.write('in:    %5d (%3d,%c)\n' % (a, ord(c), c) )
         self.reg[a] = ord(c)
         self.pc += 2
     
     # noop: 21
     def noop(self, tr):
-        tr.write('noop\n')
+        if self.trace:
+            tr.write('noop\n')
         self.pc += 1
   
     
@@ -238,16 +261,34 @@ class Vm:
                 self.readin,
                 self.noop ]
 
-        with open('trace', 'w') as tr:
+        with open('/Volumes/RAMDisk/trace', 'w') as tr:
             while self.pc < MAX:
                 i = self.mem[self.pc]
-                # tr.write('reg: %32s [%d] %d=' % (self.reg, self.pc, i))
-                tr.write('[%5d] %2d=' % (self.pc, i))
+
+                if self.trace:
+                    tr.write('[%5d] %2d=' % (self.pc, i))
+
                 if  i < len(fn_table):
                     fn_table[i](tr)
                 else:
                     self.pc = MAX
                     print('Unknown op code: ' + str(i))
+                
+#               with open('/Volumes/RAMDisk/memdmp', 'w') as memdmp:# {{{
+#                   row_len = 8
+#                   memdmp.write('PC: %d\n' % self.pc)
+#                   memdmp.write('Reg: %s\n' % self.reg)
+#                   memdmp.write('Mem:\n')
+#                   for i in range(0, int(len(self.mem)/row_len)):
+#                       memdmp.write('%5d: ' % (i * row_len))
+#                       for j in range(0, row_len):
+#                           v = self.mem[i * row_len + j]
+#                           c = chr(v)
+#                           if c < 'A' or  c > 'z':
+#                               c = ''
+#                           memdmp.write('%5d [%2s] ' % (v, c))
+#                       memdmp.write('\n')# }}}
+
     
     
 # prog = [9,32768,32769,4,19,32768]
